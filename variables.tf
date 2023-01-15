24fe0d0af6d6848cdb2ba6ab8479add9ae5782dd
John Doe <john.doe@contoso.com>
2023-01-15 18:48:49 -0500

    fix : update

--- variables.tf
+++ variables.tf
@@ -1,19 +1,12 @@
-
 variable "location" {
   type        = string
   description = "The primary location for our resources"
   default     = "eastus2"
 }
 
-# variable "rg_name" {
-#     type = string
-#     default = "rg-${random_uuid}"
-# }
-
 variable "storage_acct_name" {
   type        = string
   description = "The storage account for our stuff"
-
 }
 
 variable "container_name" {
