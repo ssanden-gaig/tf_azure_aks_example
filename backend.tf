73773b0f9c10c41600c56e497f62076cf753ed2c
John Doe <john.doe@contoso.com>
2023-01-15 18:21:36 -0500

    init : added tf files

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }  
+  }
+
+  cloud {
+    organization = "example-org-3573a2"
+
+    workspaces {
+      name = "AzureTest2"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+
+variable "location" {
+    type = string
+    description = "The primary location for our resources"
+    default = "eastus2"
+}
+
+# variable "rg_name" {
+#     type = string
+#     default = "rg-${random_uuid}"
+# }
+
+variable "storage_acct_name" {
+    type = string
+    description = "The storage account for our stuff"
+    
+}
+
+variable "container_name" {
+    type = string
+    description = "The container name"
+    default = "mydocs"
+}
