ab21b4f1b9da86bacd2624f27072ccb28d422ae5
John Doe <john.doe@contoso.com>
2023-01-15 18:28:02 -0500

    fix : updated format

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
 
 provider "azurerm" {
   features {}
-    }
+}
 
 resource "random_uuid" "test" {}
 
 locals {
-    containers = ["mydocs","mycode","mypics"]
-    resource_group="${random_uuid.test.result}-rg" 
+  containers     = ["mydocs", "mycode", "mypics"]
+  resource_group = "${random_uuid.test.result}-rg"
 }
 
 
 resource "azurerm_resource_group" "storage_rg" {
- location = var.location
- name = local.resource_group 
+  location = var.location
+  name     = local.resource_group
 }
 
 resource "azurerm_storage_account" "storage_acct" {
- location = azurerm_resource_group.storage_rg.location
- name = var.storage_acct_name
- account_tier = "Standard"
- account_replication_type = "LRS"
- resource_group_name=azurerm_resource_group.storage_rg.name
+  location                 = azurerm_resource_group.storage_rg.location
+  name                     = var.storage_acct_name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  resource_group_name      = azurerm_resource_group.storage_rg.name
 }
 
 resource "azurerm_storage_container" "storage_container" {
-  for_each = toset(local.containers)  
-  
+  for_each = toset(local.containers)
+
   storage_account_name = azurerm_storage_account.storage_acct.name
-  name= "${each.value}"
+  name                 = each.value
 
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 
 variable "location" {
-    type = string
-    description = "The primary location for our resources"
-    default = "eastus2"
+  type        = string
+  description = "The primary location for our resources"
+  default     = "eastus2"
 }
 
 # variable "rg_name" {
@@ -11,13 +11,13 @@ variable "location" {
 # }
 
 variable "storage_acct_name" {
-    type = string
-    description = "The storage account for our stuff"
-    
+  type        = string
+  description = "The storage account for our stuff"
+
 }
 
 variable "container_name" {
-    type = string
-    description = "The container name"
-    default = "mydocs"
+  type        = string
+  description = "The container name"
+  default     = "mydocs"
 }
